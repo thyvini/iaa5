69beeca751408e96379a599ed7d9749ec2e3ae7f
Thyago Vinicius Camilo Cunha <thyago.vccunha@gmail.com>
2024-05-17 00:49:05 -0300

    adicionar roteiro

---
+++ iaa-5-ex-1.R
@@ -0,0 +1,18 @@
+# install packages
+# load libraries
+# load dataset
+# set seed
+# create index for data partition
+# create train database
+# create test database
+# standardize variables
+# create used variables list
+# generate dummies
+# define x_train as training matrix for independent variables
+# define y_train as training matrix for dependent variable
+# define x_test as test matrix for independent variables
+# define y_test as test matrix for dependent variable
+# adjust model configuration
+# train model
+# make predictions for the training database
+# make predictions for the test database
