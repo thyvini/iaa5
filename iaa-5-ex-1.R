d2cce3e8d1d143bc94037101cb955dc430a52714
Thyago Vinicius Camilo Cunha <thyago.vccunha@gmail.com>
2024-05-20 00:03:06 -0300

    corrigir erros

--- iaa-5-ex-1.R
+++ iaa-5-ex-1.R
@@ -13,10 +13,10 @@ load("./trabalhosalarios.RData")
 data <- as.data.frame(trabalhosalarios[, !names(trabalhosalarios) %in% c('earns')])
 
 # set seed
-set.seed(404)
+set.seed(123)
 
 # create index for data partition
-index <- sample(1:nrow(data), 0.75 * nrow(data))
+index <- sample(1:nrow(data), 0.8 * nrow(data))
 
 # create train database
 train <- data[index,]
@@ -29,8 +29,8 @@ numeric_columns <- c('husage', 'husearns', 'huseduc', 'hushrs',
                      'age', 'educ', 'exper')
 pre_proc_val <- preProcess(train[,numeric_columns],
                            method = c("center", "scale"))
-train[,numeric_columns] = predict(pre_proc_val, train[,numeric_columns])
-test[,numeric_columns] = predict(pre_proc_val, test[,numeric_columns])
+train[,numeric_columns] <- predict(pre_proc_val, train[,numeric_columns])
+test[,numeric_columns] <- predict(pre_proc_val, test[,numeric_columns])
 
 # create used variables list
 columns <- c('husage', 'husunion', 'husearns',
@@ -46,20 +46,20 @@ dummies <- dummyVars(lwage ~ husage + husunion + husearns
                      + black + educ + hispanic 
                      + union + exper + kidlt6,
                      data = data[,columns])
-train_dummies = predict(dummies, newdata = train[,columns])
-test_dummies = predict(dummies, newdata = test[,columns])
+train_dummies <- predict(dummies, newdata = train[,columns])
+test_dummies <- predict(dummies, newdata = test[,columns])
 
 # define x_train as training matrix for independent variables
-x_train = as.matrix(train_dummies)
+x_train <- as.matrix(train_dummies)
 
 # define y_train as training matrix for dependent variable
-y_train = train$lwage
+y_train <- train$lwage
 
 # define x_test as test matrix for independent variables
-x_test = as.matrix(test_dummies)
+x_test <- as.matrix(test_dummies)
 
 # define y_test as test matrix for dependent variable
-y_test = test$lwage
+y_test <- test$lwage
 
 # adjust models configuration
 lambdas <- 10 ^ seq(2, -3, by = -.1)
@@ -73,14 +73,14 @@ elastic_train_control <-
   trainControl(method = "repeatedcv", number = 10, repeats = 5,
                search = "random", verboseIter = TRUE)
 
-train_formula = as.formula(lwage ~ husage + husunion + husearns
+train_formula <- as.formula(lwage ~ husage + husunion + husearns
                            + huseduc + husblck + hushisp
                            + hushrs + kidge6 + age
                            + black + educ + hispanic 
                            + union + exper + kidlt6)
 
 # train models
-ridge <- glmnet(x_train, y_train, nlambda = 25, alpha = 0, 
+ridge <- glmnet(x_train, y_train, nlambda = 25, alpha = 0,
                family = 'gaussian', lambda = best_ridge_lambda)
 lasso <- glmnet(x_train, y_train, alpha = 1, lambda = best_lasso_lambda,
                standardize = TRUE)
@@ -95,7 +95,7 @@ evaluate_results <- function(true, predicted, df) {
   SSE <- sum((predicted - true) ^ 2)
   SST <- sum((true - mean(true)) ^ 2)
   R_square <- 1 - SSE / SST
-  RMSE = sqrt(SSE / nrow(df))
+  RMSE <- sqrt(SSE / nrow(df))
   
   return(data.frame(
     RMSE = RMSE,
@@ -151,7 +151,7 @@ new_predict.lasso <- predict(lasso, s = best_lasso_lambda, new_data)
 new_predict.elastic <- predict(elastic, new_data)
 
 normalize_wage <- function(lwage) {
-  return((lwage * sd(data$lwage)) + mean(data$lwage))
+  return((lwage * sd(train$lwage)) + mean(train$lwage))
 }
 
 results.ridge.new <- normalize_wage(new_predict.ridge)
@@ -162,9 +162,32 @@ n <- nrow(train) # sample size
 s <- sd(data$lwage) # standard deviation
 dam <- s/sqrt(n) # sample mean distribution
 
-results.ridge.new.ci.upper <- results.ridge.new + (qnorm(0.025)) * dam
-results.ridge.new.ci.lower <- results.ridge.new - (qnorm(0.025)) * dam
-results.lasso.new.ci.upper <- results.lasso.new + (qnorm(0.025)) * dam
-results.lasso.new.ci.lower <- results.lasso.new - (qnorm(0.025)) * dam
-results.elastic.new.ci.upper <- results.elastic.new + (qnorm(0.025)) * dam
-results.elastic.new.ci.lower <- results.elastic.new - (qnorm(0.025)) * dam
+# confidence intervals
+results.ridge.new.ci.lower <- results.ridge.new + (qnorm(0.025)) * dam
+results.ridge.new.ci.upper <- results.ridge.new - (qnorm(0.025)) * dam
+results.lasso.new.ci.lower <- results.lasso.new + (qnorm(0.025)) * dam
+results.lasso.new.ci.upper <- results.lasso.new - (qnorm(0.025)) * dam
+results.elastic.new.ci.lower <- results.elastic.new + (qnorm(0.025)) * dam
+results.elastic.new.ci.upper <- results.elastic.new - (qnorm(0.025)) * dam
+
+# create tables with results
+tb_stats <- data.frame(
+  c('ridge', 'lasso', 'elastic'),
+  c(results.ridge.train$Rsquare, results.lasso.train$Rsquare, results.elastic.train$Rsquare),
+  c(results.ridge.train$RMSE, results.lasso.train$RMSE, results.elastic.train$RMSE),
+  c(results.ridge.test$Rsquare, results.lasso.test$Rsquare, results.elastic.test$Rsquare),
+  c(results.ridge.test$RMSE, results.lasso.test$RMSE, results.elastic.test$RMSE)
+)
+             
+colnames(tb_stats) <- c('model', 'r2 (train)', 'rmse (train)', 'r2 (test)', 'rmse (test)')
+View(tb_stats)
+
+tb_predict <- data.frame(
+  c('ridge', 'lasso', 'elastic'),
+  c(results.ridge.new, results.lasso.new, results.elastic.new),
+  c(results.ridge.new.ci.lower, results.lasso.new.ci.lower, results.elastic.new.ci.lower),
+  c(results.ridge.new.ci.upper, results.lasso.new.ci.upper, results.elastic.new.ci.upper)
+)
+
+colnames(tb_predict) <- c('model', 'predict', 'CI-lower', 'CI-upper')
+View(tb_predict)
